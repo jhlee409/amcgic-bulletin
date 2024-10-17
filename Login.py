b64f63564473a05d2e32e04fbe70e3709144889d
jhlee409 <jhlee409@gmail.com>
2024-10-17 15:55:37 +0900

    ㄷ

--- Login.py
+++ Login.py
@@ -34,7 +34,7 @@ if not st.session_state.logged_in:
     # 로그인 버튼이 클릭되었을 때만 처리
     if login_button:
         # ID 및 비밀번호 확인
-        is_login = id == "amcgi" and password == "3986"
+        is_login = id == "amcgi" and password == "3180"
 
         if is_login:
             # 로그인 성공 시 처리
