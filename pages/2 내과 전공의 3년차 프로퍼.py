a2de6096e9e3acf342c332367b225fc5e67fcc36
jhlee409 <jhlee409@gmail.com>
2025-01-04 14:27:28 +0900

    42

--- pages/2 내과 전공의 3년차 프로퍼.py
+++ pages/2 내과 전공의 3년차 프로퍼.py
@@ -28,13 +28,13 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
             1. 4월 orientation과 memory test 를 시작으로 앞에 첨부된 연간 일정표에 따라 훈련이 진행됩니다. OT 때 교육 수첩을 받아 교육과정 내내 사용하게 됩니다.
             1. 교육 과정은 MT(memory test), SHT(scope handling test), EMT(EGD method training), participation, performance test로 이 순서에 따라 진행됩니다.
             1. 훈련 프로그램 OT는 1번을 기본으로 하고, 필요에 따라 2번 반복하는 것이 원칙이니, 미리 알려주는 일정에 참여할 수 있도록 스케쥴을 조정하기 바랍니다.
-            1. 훈련과정의 소개 프로그램은 [Sim Class](https://edu.amc.seoul.kr/)에 접속하면 시청할 수 있습니다. 구체적인 방법은 앞의 Home page를 참고하세요
+            1. 훈련과정의 소개 프로그램은 [AMC GI 상부 Simulator training](https://amcgic-simulator.streamlit.app/)에 접속하면 시청할 수 있습니다. 구체적인 방법은 앞의 Home page를 참고하세요
             1. 시뮬레이션 센터의 여러 선생님들이 소화기의 요청에 의해 프로그램을 운영하시고 계십니다. 예의와 존중을 잊지 말기 바랍니다.               
         '''
         )
     with tab2:
         st.subheader("내과 3년차 proper 진단 EGD 교육 프로그램 설명")
-        st.markdown("상세한 내용은 [Sim Class](https://edu.amc.seoul.kr/)에 있는 소개 동영상을 시청하기 바랍니다.")
+        st.markdown("상세한 내용은 [AMC GI 상부 Simulator training](https://amcgic-simulator.streamlit.app/)에 있는 소개 동영상을 시청하기 바랍니다.")
         
         st.divider()
 
@@ -43,9 +43,9 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
             st.markdown(
                 '''
                 1. 이 과정은 내시경 수행 순서를 말로 외움으로써 실제 스코프를 잡고 훈련할 때 주의력이 분산되지 않도록 하는 목적으로 만들어진 프로그램입니다.
-                1. https://mt-results.streamlit.app/ 에 접속해서, 첨부된 검사 설명 문서와 이를 읽어주는 narration 파일을 다운받아 모두 암기합니다. 조사 빼고는 모두 외어야 합니다.
+                1. [AMC GI 상부 Simulator training](https://amcgic-simulator.streamlit.app/) 에 접속해서, 첨부된 검사 설명 문서와 이를 읽어주는 narration 파일을 다운받아 모두 암기합니다. 조사 빼고는 모두 외어야 합니다.
                 1. 그냥 막연하게 외우려면 쉽지 않습니다. 우선 전문가의 EGD 수행 해설 동영상 2개를 다운 받아 충분히 보세요 이해도 빨라지고, 암기에도 도움이 됩니다.
-                1. 다 암기 했다고 자신이 생기면 웹캠 앞에서 외우는 동영상을 녹화하고 이를 올리면 교수진이 평가하여 합격 불합격을 통보 하게 됩니다.
+                1. 다 암기 했다고 자신이 생기면 웹캠 앞에서 외우는 동영상을 녹화하고 이를 앞에서 말한 웹페이지에 올리면 됩니다.
                 '''
             )
             
@@ -54,9 +54,11 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
             st.markdown(
                 '''
                 1. 이 프로그램은 간략화된 시뮬레이터를 대상으로 훈련하여 내시경 조작 technique을 집중적으로 익히도록 고안된 프로그램입니다.
-                1. 벽에 자석으로 붙어있는 7 개의 단추를 2분 30초 안에, 도중에 떨어 뜨리지 않고, 정해진 용기에 운반해 넣으면 합격입니다.
+                1. 벽에 자석으로 붙어있는 7 개의 단추를 정해진 시간 안에, 도중에 떨어 뜨리지 않고, 정해진 용기에 운반해 넣으면 합격입니다.
                 1. 이 때부터 스코프를 직접 다루게 되는데 무리한 동작으로 스코프가 고장나지 않게 조심하는 습관을 가지기 바랍니다.
+                1. 안내 및 시범 동영상은 [AMC GI 상부 Simulator training](https://amcgic-simulator.streamlit.app/)에서 다운 받아 보세요.
                 1. 적어도 이 과정은 이수해야 실제 내시경 검사에서 스코프를 잡을 수 있습니다. 환자 안전을 위해 그 전에는 참관만 하세요.
+                1. 자신이 생기면 수행과정을 동영상으로 녹화하여 앞에 말한 웹페이지에 제출하면 됩니다.
                 '''
             )
         st.subheader("EGD Method Trainig")
@@ -66,9 +68,9 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
                 1. 이 과정은 인체와 똑같은 3차원 구조와 경도를 가진 실리콘 모형을 대상으로 EGD를 체계적으로 수행하는 훈련을 하는 과정입니다.
                 1. 여러분이 암기한 검사 순서를 이 시뮬레이터를 대상으로 해 보면서 숙련되도록 하는데, 중요한 점은 천천히 같은 속도록 사진도 찍어가며 정해진 순서를 지키는 것입니다.
                 1. 전체 검사 시간은 5분에서 5분30초 사이로, 사진은 62장에서 66장 사이의 사진을 찍어야 합니다.
-                1. 자신이 있으면 https://project-emt-skill-evaluation.streamlit.app/ 에 수행 동영상을 올립니다.
-                1. 머신 러닝 프로그램이 검사 수행 숙련도에 따라 합격과 불합격을 판정합니다. 합격이면 자료가 교수진에게 자동으로 전송됩니다.
-                1. 불합격이면 자료가 전송되지 않습니다. 열심히 훈련하여 모두 합격을 받아야 합니다.
+                1. [AMC GI 상부 Simulator training](https://amcgic-simulator.streamlit.app/)에 훈련에 도움이 되는 자료가 있으므로 다운 받아 보세요.
+                1. 자신이 있으면 동영상으로 저장하여 위에서 말한 웹페이지에 올리면 자동으로 합격 불합격을 판정해 줍니다. 합격하면 자동으로 보고서가 제출되고 훈련이 종료됩니다.
+                1. 불합격이면 자료가 전송되지 않습니다. 다시 도전해야 합니다. 열심히 훈련하여 모두 합격되기를 바랍니다.
                 '''
             )
         st.subheader("Participation")
