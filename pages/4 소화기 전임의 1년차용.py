a94deb2155c062db5faed9d15a25ed2b69390f39
jhlee409 <jhlee409@gmail.com>
2025-01-04 14:35:11 +0900

    44

--- pages/4 소화기 전임의 1년차용.py
+++ pages/4 소화기 전임의 1년차용.py
@@ -29,8 +29,8 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
             1. 근무 초기에는 다른 병원에서 수련받다가 온 선생님들이 여기 시스템에 적응하는데 힘들어하는 시기 입니다. 잘 도와주기 바랍니다.
             1. 출신을 불문하고 EGD 기초 교육을 이수하지 못한 선생님들은 3월 중에 반드시 교육과정을 이수하기 바랍니다. 소화기 과내 방침입니다.
             1. 진단 EGD를 빨리 배우고 싶은 마음은 이해됩니다. 그러나 ER, 강릉, GI B 등의 일정도 있기 때문에 일정 정하는데 갈등이 있을 수 있겠습니다. 과내 지침은 초반에는 주로 원내 출신이 앞의 일정을 맡고 그 다음 달에는 앞의 일정은 빠진다는 것입니다. 모쪼록 원내 출신 선생님들의 자발적인 협조를 부탁합니다.
-            1. [GI training program](https://gi-training.streamlit.app/)에 올려져 있는 교육 자료들은 매우 충실하게 잘 만든 교육자료 입니다. 시기별로 자기에게 맞는 내용을 꼭 반복해서 학습하세요.
-            1. GI training program는 출석 체크가 되므로, 끝낼 때는 반드시 왼쪽 아래 로그아웃 버튼을 눌러 종결하세요. 그냥 나가면 출석처리가 안됩니다.
+            1. [AMC GI 상부 Education Program](https://amcgic-training.streamlit.app/)에 올려져 있는 교육 자료들은 매우 충실하게 잘 만든 교육자료 입니다. 시기별로 자기에게 맞는 내용을 꼭 반복해서 학습하세요.
+            1. AMC GI 상부 Education Program은 출석 체크가 되므로, 끝낼 때는 반드시 왼쪽 아래 로그아웃 버튼을 눌러 종결하세요. 그냥 나가면 출석처리가 안됩니다.
             1. AI patient Hx. taking 훈련 프로그램은 매우 강력한 프로그램입니다. 완전히 자기 것이 될 정도로 훈련하면 외래 진료를 능숙하게 할 수 있게 될겁니다.
             1. EGD Lesion Dx. 훈련 프로그램은 매우 강력한 프로그램입니다. 여러분이 알아야 할 거의 모든 증례를 다룹니다. 완전히 자기 것이 될 정도로 훈련하면 실력향상에 큰 도움이 되니 열심히 하세요.
         '''
@@ -83,7 +83,7 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
                 '''
                 1. 기본 시뮬레이터 내시경 교육을 받고 나서 1-2 달이 지나면 조금은 내시경 조작에 익숙해지지만, 환자의 다양한 상황에 대해 대처를 못해 애를 먹게됩니다.
                 1. 이 강의 동영상은 예를 들면 'pyloric ring을 통과하는데 잘 안된다'와 같은 다양하고 흔한 상황에서 전문가가 어떻게 하는지를 나래이션으로 설명하는 동영상클립들을 모아 놓는 프로그램입니다.
-                1. 강의 동영상은 [GI training program](https://gi-training.streamlit.app/)에 올려져 있고, 보통은 5월이나 6월 정도면 절실하게 필요한 내용이 들어 있습니다.
+                1. 강의 동영상은 [AMC GI 상부 Education Program](https://amcgic-training.streamlit.app/)에 올려져 있고, 보통은 5월이나 6월 정도면 절실하게 필요한 내용이 들어 있습니다.
                 1. 1년차 첫 달에는 더 기본적인 조작이 힘들어 이 동영상을 봐도 먼나라 얘기이고, 1년차 후반부에는 볼 필요가 없을 정도로 이미 숙련되어 있을 것이므로 꼭 필요한 때 찾아서 보기 바랍니다.
                 '''
             )
@@ -93,7 +93,7 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
                 '''
                 1. AMC GI 에서 시행되는 지혈술 시뮬레이터 훈련 종목은, hemoclip, injection, APC, Nexpower spray, EVL  이렇게 5가지 입니다.
                 1. 7월 전반부에 주로 토요일 오후에 교육이 있습니다. 5개 방에서 모든 지혈술을 그 날 훈련하게 됩니다. 확정 날짜는 추후 통보됩니다.
-                1. 훈련 전 [GI training program](https://gi-training.streamlit.app/)에 올려져 있는 EGD Hemostasis 강의 동영상을 충분히 보고 훈련에 임하기 바랍니다. 특정 종목에서는 협찬한 회사에서 사전 발표를 하는 경우도 있겠습니다.
+                1. 훈련 전 [AMC GI 상부 Education Program](https://amcgic-training.streamlit.app/)에 올려져 있는 EGD Hemostasis 강의 동영상을 충분히 보고 훈련에 임하기 바랍니다. 특정 종목에서는 협찬한 회사에서 사전 발표를 하는 경우도 있겠습니다.
                 1. 5개 그룹으로 나누어서 한 방에 한 그룹이 들어가 훈련하고 로테이션 하는 체제로 진행합니다.
                 '''
             )
@@ -102,7 +102,7 @@ if st.session_state.get('logged_in', False):  # 로그인 상태 확인
             st.markdown(
                 '''
                 1. 9월 초에 PEG simulator를 가지고 직접 해보는 훈련이 있습니다. 대게는 토요일 오후에 시행하게 됩니다. 확정 날짜는 추후 통보됩니다.
-                1. 훈련 전 [GI training program](https://gi-training.streamlit.app/)에 올려져 있는 PEG 강의 동영상을 충분히 보고 훈련에 임하기 바랍니다.
+                1. 훈련 전 [AMC GI 상부 Education Program](https://amcgic-training.streamlit.app/)에 올려져 있는 PEG 강의 동영상을 충분히 보고 훈련에 임하기 바랍니다.
                 1. 세 조로 나뉘어 각 방에서 시범을 보고 직접 해 보는 방식으로 진행됩니다.
                 '''
             )
